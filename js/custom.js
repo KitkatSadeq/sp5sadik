a04dc0c1033643bfb7bcb682e53a8111d584c68f
KitkatSadeq <100295235+KitkatSadeq@users.noreply.github.com>
2022-11-26 03:38:25 +0600

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,25 @@
+// $(select).action()
+
+/* $(window).scroll(function(){
+    $(".menu").addClass("bgmenu jddgg")
+})
+
+
+$(".btn1").click(function(){
+    $(".one").hide(500)
+})
+$(".btn2").click(function(){
+    $(".one").show(500)
+})
+$(".btn3").click(function(){
+    $(".one").toggle(500)
+})
+$(".btn4").click(function(){
+    $(".two").fadeOut(1000)
+})
+$(".btn5").click(function(){
+    $(".two").fadeIn(1000)
+})
+$(".btn6").click(function(){
+    $(".two").fadeToggle(1000)
+}) */
